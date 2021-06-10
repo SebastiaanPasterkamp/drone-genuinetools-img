6cf8c226e092d29b35779b8610d327e6fbd3cc6a
Sebastiaan Pasterkamp <26205277+SebastiaanPasterkamp@users.noreply.github.com>
2021-06-10 21:08:38 +0200

    plugin.sh: Update to match genuinetools/img CLI args

--- plugin.sh
+++ plugin.sh
@@ -4,6 +4,8 @@
 set -euo pipefail
 export PATH=/usr/bin:$PATH
 
+img --version
+
 REGISTRY=${PLUGIN_REGISTRY:-index.docker.io}
 
 if [ "${PLUGIN_USERNAME:-}" ] || [ "${PLUGIN_PASSWORD:-}" ]; then
@@ -28,17 +30,16 @@ fi
 DOCKERFILE=${PLUGIN_DOCKERFILE:-Dockerfile}
 CONTEXT=${PLUGIN_CONTEXT:-$PWD}
 LOG=${PLUGIN_LOG:-info}
-EXTRA_OPTS=""
 
 if [[ -n "${PLUGIN_TARGET:-}" ]]; then
     TARGET="--target=${PLUGIN_TARGET}"
 fi
 
-if [[ "${PLUGIN_SKIP_TLS_VERIFY:-}" == "true" ]]; then
-    EXTRA_OPTS="--insecure-registry=true"
+if [[ "${PLUGIN_INSECURE_REGISTRY:-}" == "true" ]]; then
+    INSECURE_REGISTRY="--insecure-registry"
 fi
 
-if [[ "${PLUGIN_CACHE:-}" == "false" ]]; then
+if [[ "${PLUGIN_CACHE:-}" != "true" ]]; then
     CACHE="--no-cache"
 fi
 
@@ -50,6 +51,20 @@ if [ -n "${PLUGIN_BUILD_ARGS:-}" ]; then
     BUILD_ARGS="--build-arg=${PLUGIN_BUILD_ARGS}"
 fi
 
+if [ -n "${PLUGIN_PLATFORM:-}" ]; then
+    PLATFORM=$(
+        echo "${PLUGIN_PLATFORM}" | tr ',' '\n' | while read platform; do
+            echo "--platform=${platform} ";
+        done
+    )
+fi
+
+IMAGE=""
+if [ -n "${PLUGIN_REGISTRY:-}" ] && [ -n "${PLUGIN_REPO:-}" ]; then
+    IMAGE="${PLUGIN_REGISTRY}/${PLUGIN_REPO}"
+fi
+
+TAGS="--tag=${IMAGE}:latest"
 # auto_tag, if set auto_tag: true, auto generate .tags file
 # support format Major.Minor.Release or start with `v`
 # docker tags: Major, Major.Minor, Major.Minor.Release and latest
@@ -57,12 +72,12 @@ if [[ "${PLUGIN_AUTO_TAG:-}" == "true" ]]; then
     TAG=$(echo "${DRONE_TAG:-}" |sed 's/^v//g')
     part=$(echo "${TAG}" |tr '.' '\n' |wc -l)
     # expect number
-    echo ${TAG} |grep -E "[a-z-]" &>/dev/null && isNum=1 || isNum=0
+    echo ${TAG} | grep -E "[a-z-]" &> /dev/null && isNum=1 || isNum=0
 
     if [ ! -n "${TAG:-}" ];then
-        echo "latest" > .tags
+        TAGS="--tag=${IMAGE}:latest"
     elif [ ${isNum} -eq 1 -o ${part} -gt 3 ];then
-        echo "${TAG},latest" > .tags
+        TAGS="--tag=${IMAGE}:${TAG} --tag=${IMAGE}:latest"
     else
         major=$(echo "${TAG}" |awk -F'.' '{print $1}')
         minor=$(echo "${TAG}" |awk -F'.' '{print $2}')
@@ -72,16 +87,30 @@ if [[ "${PLUGIN_AUTO_TAG:-}" == "true" ]]; then
         minor=${minor:-0}
         release=${release:-0}
 
-        echo "${major},${major}.${minor},${major}.${minor}.${release},latest" > .tags
+        TAGS="--tag=${IMAGE}:${major} --tag=${IMAGE}:${major}.${minor} --tag=${IMAGE}:${major}.${minor}.${release} --tag=${IMAGE}:latest"
     fi
+elif [ -n "${PLUGIN_TAGS:-}" ]; then
+    TAGS=$(
+        echo "${PLUGIN_TAGS}" | tr ',' '\n' | while read tag; do
+            echo "--tag=${IMAGE}:${tag} "
+        done
+    )
 fi
 
+echo "Building '${DOCKERFILE}' in '${CONTEXT}' as ${TAGS//--tag=/}"
+
 /usr/bin/img \
-    --context=${CONTEXT} \
-    --dockerfile=${DOCKERFILE} \
-    ${EXTRA_OPTS} \
-    ${DESTINATIONS} \
+    build \
+    --file=${DOCKERFILE} \
+    ${PLATFORM:-} \
     ${CACHE:-} \
     ${CACHE_REPO:-} \
-    ${TARGET:-} \
-    ${BUILD_ARGS:-}
+    ${BUILD_ARGS:-} \
+    ${TAGS} \
+    ${CONTEXT}
+
+echo "Pushing ${INSECURE_REGISTRY} ${TAGS//--tag=}"
+
+echo "${TAGS//--tag=/}" | while read -d ' ' tag; do
+    /usr/bin/img push "${INSECURE_REGISTRY:-}" "${tag}"
+done
